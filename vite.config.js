abf6353764fb441f785b7974636b6e22879d7555
mouzzammehmoood <mouzzammehmood8@gmail.com>
2026-09-10 16:03:23 +0400

    Update Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "First-React-App",
   plugins: [react(), tailwindcss()],
 });
